7825e111ccb074633acac22b4892318c619ae4d3
Mahmoud Mohamed Abouelala <59162393+mahmoud206@users.noreply.github.com>
2025-04-02 04:08:45 +0300

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:vet_reports_app/app/app.dart';
+import 'package:vet_reports_app/main.dart'; 
 
 
 void main() {
