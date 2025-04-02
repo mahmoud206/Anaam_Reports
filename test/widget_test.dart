144f87418b3d4d5e666862d5d09dc133b52858f8
Mahmoud Mohamed Abouelala <59162393+mahmoud206@users.noreply.github.com>
2025-04-02 03:45:50 +0300

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,43 +4,27 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:vet_reports_app/main.dart';
-import 'mocks.dart';
+import 'package:vet_reports_app/app/app.dart';
+
 
 void main() {
-  late MockMongoService mockMongoService;
-  late MockPdfService mockPdfService;
-
-  setUp(() {
-    mockMongoService = MockMongoService();
-    mockPdfService = MockPdfService();
-  });
-
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(VetReportsApp(
-      mongoService: mockMongoService,
-      pdfService: mockPdfService,
-    ));
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const VetReportsApp());
 
-    // Verify initial state
+    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap '+' button
+    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify increment
+    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
-
-
-
-
-
-   
-
